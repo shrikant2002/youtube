7feb3d51069ba605c718dc0034c22714e68e094e
Shrikant Nimbalkar <shrikantdnimbalkar@gmail.com>
2020-06-06 19:19:27 +0530

    Update play.bat

--- play.bat
+++ play.bat
@@ -1,9 +1,9 @@
 #saleem
 start chrome "https://www.youtube.com/watch?v=yJTBQm91z20&list=RDMM&start_radio=1"
-start firefox "https://www.youtube.com/watch?v=yJTBQm91z20&list=RDMM&start_radio=1"
+start chrome "https://www.youtube.com/watch?v=nHdJzeoESaY&list=PLtVtqA0Eil0NqDM1z59KOpWXv5Avt9vvN"
+
 
 #salman2
-start chrome "https://www.youtube.com/watch?v=nHdJzeoESaY&list=PLtVtqA0Eil0NqDM1z59KOpWXv5Avt9vvN"
-start firefox "https://www.youtube.com/watch?v=nHdJzeoESaY&list=PLtVtqA0Eil0NqDM1z59KOpWXv5Avt9vvN"
-
+#start firefox "https://www.youtube.com/watch?v=nHdJzeoESaY&list=PLtVtqA0Eil0NqDM1z59KOpWXv5Avt9vvN"
+#start firefox "https://www.youtube.com/watch?v=yJTBQm91z20&list=RDMM&start_radio=1"
               
