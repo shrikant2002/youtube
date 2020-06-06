e15652e5cd7d1a9b53658f8f4caa27e46e74df2d
Shrikant Nimbalkar <shrikantdnimbalkar@gmail.com>
2020-06-06 19:17:37 +0530

    Update play.bat

--- play.bat
+++ play.bat
@@ -1,23 +1,9 @@
-#All Videos
-start chrome "https://www.youtube.com/watch?v=82-MDymVkps&list=PLAQHyp2rW3780t9P_DzZ_3V3i8BGwc7y9"
-start firefox "https://www.youtube.com/watch?v=82-MDymVkps&list=PLAQHyp2rW3780t9P_DzZ_3V3i8BGwc7y9"
+#saleem
+start chrome "https://www.youtube.com/watch?v=yJTBQm91z20&list=RDMM&start_radio=1"
+start firefox "https://www.youtube.com/watch?v=yJTBQm91z20&list=RDMM&start_radio=1"
 
-#Chrome OS
-start chrome "https://www.youtube.com/watch?v=4gZYV0RWJQ8&list=PLAQHyp2rW379mp7tXvBnSBMKkBY5QfC3E"
-start firefox "https://www.youtube.com/watch?v=4gZYV0RWJQ8&list=PLAQHyp2rW379mp7tXvBnSBMKkBY5QfC3E"
+#salman2
+start chrome "https://www.youtube.com/watch?v=nHdJzeoESaY&list=PLtVtqA0Eil0NqDM1z59KOpWXv5Avt9vvN"
+start firefox "https://www.youtube.com/watch?v=nHdJzeoESaY&list=PLtVtqA0Eil0NqDM1z59KOpWXv5Avt9vvN"
 
-#Infrared Camera
-start chrome "https://www.youtube.com/watch?v=AGiDtOsa8VI&list=PLAQHyp2rW378mXKEtVfroaTCK_Wtxhoj3"
-start firefox "https://www.youtube.com/watch?v=AGiDtOsa8VI&list=PLAQHyp2rW378mXKEtVfroaTCK_Wtxhoj3"
-
-#EMP Devices
-start chrome "https://www.youtube.com/watch?v=Q4WzhlJmRD8&list=PLAQHyp2rW378r39L7VruL6n9l9vZynZso"
-start firefox "https://www.youtube.com/watch?v=AGiDtOsa8VI&list=PLAQHyp2rW378mXKEtVfroaTCK_Wtxhoj3"
-
-#Wifi Jammer
-start chrome "https://www.youtube.com/watch?v=41bOZaO7Tgo&list=PLAQHyp2rW37_j0YWSbzEzW2c4nMbQGj4Q"
-start firefox "https://www.youtube.com/watch?v=AGiDtOsa8VI&list=PLAQHyp2rW378mXKEtVfroaTCK_Wtxhoj3"
-
-#Usb Killer
-start chrome "https://www.youtube.com/watch?v=OBGp8OeZdLk&list=PLAQHyp2rW37-oBw1T24r8J0j514U5EE3Y"
-start firefox "https://www.youtube.com/watch?v=AGiDtOsa8VI&list=PLAQHyp2rW378mXKEtVfroaTCK_Wtxhoj3"
+              
