1194b5d6bced32eb4c9af3d8e38a1b83fe6ec632
Shrikant Nimbalkar <shrikantdnimbalkar@gmail.com>
2020-06-06 19:31:24 +0530

    Update play.bat

--- play.bat
+++ play.bat
@@ -1,9 +1,11 @@
-#saleem
-start chrome "https://www.youtube.com/watch?v=yJTBQm91z20&list=RDMM&autoplay=1&start_radio=1"
-start chrome "https://www.youtube.com/watch?v=nHdJzeoESaY&autoplay=1&list=PLtVtqA0Eil0NqDM1z59KOpWXv5Avt9vvN"
+rem saleem
+"c:\Program Files\Google\Chrome\Application\chrome.exe" --app="https://www.youtube.com/watch?v=yJTBQm91z20&list=RDMM&autoplay=1&start_radio=1"
+"c:\Program Files\Google\Chrome\Application\chrome.exe" --app="https://www.youtube.com/watch?v=nHdJzeoESaY&autoplay=1&list=PLtVtqA0Eil0NqDM1z59KOpWXv5Avt9vvN"
+rem start chrome "https://www.youtube.com/watch?v=yJTBQm91z20&list=RDMM&autoplay=1&start_radio=1"
+rem start chrome "https://www.youtube.com/watch?v=nHdJzeoESaY&autoplay=1&list=PLtVtqA0Eil0NqDM1z59KOpWXv5Avt9vvN"
 
 
-#salman2
-#start firefox "https://www.youtube.com/watch?v=nHdJzeoESaY&list=PLtVtqA0Eil0NqDM1z59KOpWXv5Avt9vvN"
-#start firefox "https://www.youtube.com/watch?v=yJTBQm91z20&list=RDMM&start_radio=1"
+rem salman2
+rem start firefox "https://www.youtube.com/watch?v=nHdJzeoESaY&list=PLtVtqA0Eil0NqDM1z59KOpWXv5Avt9vvN"
+rem start firefox "https://www.youtube.com/watch?v=yJTBQm91z20&list=RDMM&start_radio=1"
               
