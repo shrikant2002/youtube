d72820a814c54474fdc33a12b6ddc34c97cb98b2
Shrikant Nimbalkar <shrikantdnimbalkar@gmail.com>
2020-06-07 15:16:39 +0530

    Update play.bat

--- play.bat
+++ play.bat
@@ -9,10 +9,10 @@ rem Infrared Camera
 start firefox "https://www.youtube.com/watch?v=AGiDtOsa8VI&list=PLAQHyp2rW378mXKEtVfroaTCK_Wtxhoj3"
 
 rem EMP Devices
-start firefox "https://www.youtube.com/watch?v=AGiDtOsa8VI&list=PLAQHyp2rW378mXKEtVfroaTCK_Wtxhoj3"
+start firefox "https://www.youtube.com/watch?v=Q4WzhlJmRD8&list=PLAQHyp2rW378r39L7VruL6n9l9vZynZso"
 
 rem Wifi Jammer
-start firefox "https://www.youtube.com/watch?v=AGiDtOsa8VI&list=PLAQHyp2rW378mXKEtVfroaTCK_Wtxhoj3"
+start firefox "https://www.youtube.com/watch?v=41bOZaO7Tgo&list=PLAQHyp2rW37_j0YWSbzEzW2c4nMbQGj4Q"
 
 rem Usb Killer
-start firefox "https://www.youtube.com/watch?v=AGiDtOsa8VI&list=PLAQHyp2rW378mXKEtVfroaTCK_Wtxhoj3"
+start firefox "https://www.youtube.com/watch?v=OBGp8OeZdLk&list=PLAQHyp2rW37-oBw1T24r8J0j514U5EE3Y"
