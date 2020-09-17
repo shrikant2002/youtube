5a3347f5c00e767b7adc73ae47ec8e4c16fee133
Shrikant Nimbalkar <shrikantdnimbalkar@gmail.com>
2020-09-17 16:19:13 +0530

    Update play.bat

--- play.bat
+++ play.bat
@@ -1,17 +1,20 @@
+rem release
+start firefox "https://www.youtube.com/watch?v=5wfeIO61sSI&list=PLG5jakMh3U33gkroXEZ_R-8vhpCo6DudX"
+
 rem all videos
-start firefox "https://www.youtube.com/watch?v=82-MDymVkps&list=PLAQHyp2rW3780t9P_DzZ_3V3i8BGwc7y9"
+rem start firefox "https://www.youtube.com/watch?v=82-MDymVkps&list=PLAQHyp2rW3780t9P_DzZ_3V3i8BGwc7y9"
 
 rem chrome os
-start firefox "https://www.youtube.com/watch?v=4gZYV0RWJQ8&list=PLAQHyp2rW379mp7tXvBnSBMKkBY5QfC3E"
+rem start firefox "https://www.youtube.com/watch?v=4gZYV0RWJQ8&list=PLAQHyp2rW379mp7tXvBnSBMKkBY5QfC3E"
 
 rem Infrared Camera
-start firefox "https://www.youtube.com/watch?v=AGiDtOsa8VI&list=PLAQHyp2rW378mXKEtVfroaTCK_Wtxhoj3"
+rem start firefox "https://www.youtube.com/watch?v=AGiDtOsa8VI&list=PLAQHyp2rW378mXKEtVfroaTCK_Wtxhoj3"
 
 rem EMP Devices
-start firefox "https://www.youtube.com/watch?v=Q4WzhlJmRD8&list=PLAQHyp2rW378r39L7VruL6n9l9vZynZso"
+rem start firefox "https://www.youtube.com/watch?v=Q4WzhlJmRD8&list=PLAQHyp2rW378r39L7VruL6n9l9vZynZso"
 
 rem Wifi Jammer
-start firefox "https://www.youtube.com/watch?v=41bOZaO7Tgo&list=PLAQHyp2rW37_j0YWSbzEzW2c4nMbQGj4Q"
+rem start firefox "https://www.youtube.com/watch?v=41bOZaO7Tgo&list=PLAQHyp2rW37_j0YWSbzEzW2c4nMbQGj4Q"
 
 rem Usb Killer
-start firefox "https://www.youtube.com/watch?v=OBGp8OeZdLk&list=PLAQHyp2rW37-oBw1T24r8J0j514U5EE3Y"
+rem start firefox "https://www.youtube.com/watch?v=OBGp8OeZdLk&list=PLAQHyp2rW37-oBw1T24r8J0j514U5EE3Y"
